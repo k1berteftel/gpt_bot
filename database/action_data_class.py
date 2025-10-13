fcc19496c797f52eca2eef2bd1fa9fd244905921
k1berteftel <kkulis985@gmail.com>
2025-10-13 20:33:16 +0300

    fix

--- database/action_data_class.py
+++ database/action_data_class.py
@@ -64,7 +64,8 @@ class DataInteraction():
     async def add_deeplink(self, link: str, name: str):
         async with self._sessions() as session:
             await session.execute(insert(DeeplinksTable).values(
-                link=link
+                link=link,
+                name=name
             ))
             await session.commit()
 
